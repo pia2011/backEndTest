59a6134d0214cfa1d59dabe0491abbf2bb05703f
pia2011 <53935439+pia2011@users.noreply.github.com>
2023-02-16 14:33:04 +0900

    프로젝트 초기 설정

---
+++ src/main/java/com/cosain/trilo/TriloApplication.java
@@ -0,0 +1,13 @@
+package com.cosain.trilo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriloApplication.class, args);
+	}
+
+}
